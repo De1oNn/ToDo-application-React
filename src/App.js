623b60df59e6127e7500e7d7d3608841970be218
De1oNn <nasanjargal20040524@gmail.com>
2025-01-03 11:02:36 +0800

    commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-
+import { use } from 'react';
 import './App.css';
 import React, { useState } from 'react';
 
 function App() {
 
-  const [todo, settodo] = useState([]);
-  const [inputValue, setinputValue] = useState("");
-  const [filter, setfilter] = useState("");
+  const [todo, setTodo] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+  const [error, setError] = useState("");
+  // const [filter, setfilter] = useState("");
 
   const handleInputChange = (event) => {
-    setinputValue(event.target.value);
+    setInputValue(event.target.value);
   }
 
   const handleAddTask = () => {
-    if (inputValue.trim()) {
-      const newTask = {
-        id: Data.now(),
-        name: inputValue,
-        completed: false
-      }
+    if(inputValue.length === 0) {
+      setError('Please Enter ToDo Task');
+      return;
+    } else {
+      setTodo([...todo, inputValue]);
+      setInputValue("");
     }
-  }
+  };
+
+  
+    // if (inputValue.trim()) {
+    //   const newTask = {
+    //     id: Date.now(),
+    //     name: inputValue,
+    //     completed: false,
+    //   };
+    //   console.log(todo);
+    //   settodo([...todo, newTask]);
+    //   setinputValue("");
+    //   }
+    // };
+
+    // const handleToggleCompletion = (id) => {
+
+    // }
 
   
   return (
@@ -28,14 +46,22 @@ function App() {
       <div className="App">
         <p id='todo-header'>To-Do List</p>
         <div className='add-button-input'>
-          <input id='input' onChange={handleInputChange} placeholder='Add a new task...'/>
-          <button id='button'>Add</button>
+          <input id='input' value={inputValue} onChange={handleInputChange} placeholder='Add a new task...'/>
+          <button id='button' onClick={handleAddTask}>Add</button>
         </div>
         <div id='buttons'>
           <button id='all-button'>All</button>
           <button id='active-button'>Active</button>
           <button id='completed-button'>Completed</button>
         </div>
+        <div>
+        {error.length > 1 && <div id='error'>{error}</div>}
+        {todo.map((todo) => {
+            return <div id='newtask'>{todo}</div>
+          })}
+        
+        </div>
+        <div id='line'></div>
         <p id='task-area'>No tasks yet. Add one above!</p>
         <p id='powered'>Powered by Pinecone academy</p>
       </div>
